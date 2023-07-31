45ae07972a7d45d458afa24b01452c45b2353697
handika prasetya <125026431+handikaprstya@users.noreply.github.com>
2023-07-31 15:30:50 +0700

    firstcmmmm

--- projects/server/middleware/validator.js
+++ projects/server/middleware/validator.js
@@ -27,5 +27,53 @@ module.exports = {
         } catch (error) {
             console.log(error)
         }
-    }
+    },
+    changePass: async (req, res, next) => {
+        try {
+            await body('currentPassword').trim().notEmpty().run(req)
+            await body('newPassword').trim().notEmpty().run(req)
+            await body('confirmPassword').trim().notEmpty().equals(req.body.newPassword).withMessage('Password not match!').run(req)
+        console.log("done change pass")
+        next()
+
+        } catch (error) {
+            console.log(error)
+            res.status(405).send(error)
+        }
+    },
+    emailVeriv: async (req, res, next) =>{
+        await body('email').trim().notEmpty().isEmail().withMessage('Format Email Salah').run(req);
+        await body('newEmail').trim().notEmpty().isEmail().withMessage('Format Email Salah').run(req);
+        const validation = validationResult(req);
+          
+        if (validation.isEmpty()) {
+          next();
+        }else{
+            res.status(400).send({
+            status : false,
+            message : 'Validation Invalid',
+            error : validation.array()
+          });
+        }
+      },
+      resPass : async (req, res, next) =>{
+        await body('newPassword').trim().notEmpty().isStrongPassword({
+          minLength: 6,
+          minLowercase: 1,
+          minUppercase: 1,
+          minNumbers : 1,
+          minSymbols : 1}).run(req);
+        await body('confirmPassword').trim().notEmpty().equals(req.body.newPassword).withMessage("password not match").run(req);
+    
+        const validation = validationResult(req);
+        if (validation.isEmpty()) {
+          next();
+        }else{
+            res.status(400).send({
+            status : false,
+            message : 'Validation Invalid',
+            error : validation.array()
+          });
+        }
+      }
 }
